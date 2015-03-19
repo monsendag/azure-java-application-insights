1228b8e1842322bdbc399dfbd557bc98ef10ab63
Andreas Pedro Garcia-Vedal <andreasv@mesan.no>
2015-03-19 16:59:53 +0100

    Optimized imports

--- src/main/java/no/mesan/clouddevops/TodoApplication.java
+++ src/main/java/no/mesan/clouddevops/TodoApplication.java
@@ -1,12 +1,7 @@
 package no.mesan.clouddevops;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class TodoApplication {
