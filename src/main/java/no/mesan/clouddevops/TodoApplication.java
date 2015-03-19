03ed0bf754946d3093850dbd3961a3aeed8b42ec
Dag <me@dag.im>
2015-03-19 16:38:16 +0100

    Revert "Gjør om appen fra å kjøre med embedded Tomcat til å kunne deployeres på en eksisterende server som en war-pakke (lokalt eller i Azure)"
    This reverts commit 9ab4c059bf6ac4436da3ead9a4dfc3b2d9ad1190.

--- src/main/java/no/mesan/clouddevops/TodoApplication.java
+++ src/main/java/no/mesan/clouddevops/TodoApplication.java
@@ -1,27 +1,12 @@
 package no.mesan.clouddevops;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-public class TodoApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class TodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(applicationClass, args);
+        SpringApplication.run(TodoApplication.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-
-    private static Class<TodoApplication> applicationClass = TodoApplication.class;
 }
