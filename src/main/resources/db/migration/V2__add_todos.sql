ff2f50faa66d11042636afcec82c503b3e42a0f3
Andreas Pedro Garcia-Vedal <andreasv@mesan.no>
2015-03-19 16:54:13 +0100

    Lagt inn litt data i databasen

---
+++ src/main/resources/db/migration/V2__add_todos.sql
@@ -0,0 +1,47 @@
+INSERT INTO todos (id, title, content)
+VALUES (1, 'Kobling mot database', 'Ansvarlig: Sasidhara Rao Yanamalamanda
+Appen må kobles mot en databaseinstans i Azure.');
+
+INSERT INTO todos (id, title, content)
+VALUES (2, 'Måling av bruk i web-apps', 'Ansvarlig: Dag Einar Monsen
+
+http://azure.microsoft.com/en-us/documentation/articles/app-insights-web-track-usage/
+
+For å få testet denne må vi lage et enkelt grensesnitt som bruker REST-apiet. Her kan vi bare plukke fra http://todomvc.com/, f.eks React-versjonen: http://todomvc.com/examples/react/#/');
+
+INSERT INTO todos (id, title, content)
+VALUES (3, 'Logging og tracing', 'Ansvarlig: Mats Holm Rosbach
+
+http://azure.microsoft.com/en-us/documentation/articles/app-insights-java-trace-logs/
+
+Oppsett av Logback med SLF4J, log-kall går til Application Insights. For mer info om Logback og SLF4J, se lyntale fra JZ2013 om hvordan bytte fra SLF4J og Logback, samt hvordan log bridging fungerer: https://vimeo.com/74274217');
+
+INSERT INTO todos (id, title, content)
+VALUES (4, 'Custom Telemetry', 'Ansvarlig:
+
+http://azure.microsoft.com/en-us/documentation/articles/app-insights-web-track-usage-custom-events-metrics/
+
+Denne må undersøkes nærmere.');
+
+INSERT INTO todos (id, title, content)
+VALUES (5, 'Monitorering av tilgjengelighet og responsivitet', 'Ansvarlig: David Kai Christen Kristensen
+
+http://azure.microsoft.com/en-us/documentation/articles/app-insights-monitor-web-app-availability/');
+
+INSERT INTO todos (id, title, content)
+VALUES (6, 'Tracking av http-requests', 'Ansvarlig: Andreas Pedro Garcia-Vedal
+
+http://azure.microsoft.com/en-us/documentation/articles/app-insights-java-track-http-requests/');
+
+
+
+
+
+
+
+
+
+
+
+
+
