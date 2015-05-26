6e5d69758c6cbb57b9ab9bbe0ce641be9696fc8c
Dag <me@dag.im>
2015-05-26 19:10:37 +0200

    Legg til tjeneste for utregning av pi

--- src/main/java/no/mesan/clouddevops/rest/PiRestController.java
+++ src/main/java/no/mesan/clouddevops/rest/PiRestController.java
@@ -1,21 +1,20 @@
 package no.mesan.clouddevops.rest;
 
+import no.mesan.clouddevops.service.PiCalculator;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
-@RequestMapping(value = "/api/scale")
-public class ScaleRestController {
+@RequestMapping(value = "/api/pi")
+public class PiRestController {
 
    @RequestMapping(value = "/{decimals}", method = GET)
-    public void delete(@PathVariable final Long decimals) {
+    public BigDecimal delete(@PathVariable final int decimals) {
         // regn ut variabler for pi
-
-
-
-
+        return PiCalculator.pi(decimals);
     }
-
 
 }
